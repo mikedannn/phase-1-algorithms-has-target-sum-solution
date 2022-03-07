28f695804babdc355b12c077a47e72cccab35e55
Michael <michael.daniels1013@gmail.com>
2022-03-06 19:07:50 -0600

    commit tree e6c1fd3f7cf4e51f8e5883151034cf03aa9d4e84

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+{function hasTargetSum(array, target) {
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
+}
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
-  Add your pseudocode here
+  iterate over the array of numbers
+    for the current number, identify a complementary number that adds to our target
+    (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    iterate over the remaining numbers in the array
+      check if any of the remaining numbers is the complement
+        if so, return true
+  if we reach the end of the array, return false
 */
 
 /*
@@ -29,6 +46,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5, 8, 22], 56));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2], 100));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5, 22, 86], 10));
 }
 
 module.exports = hasTargetSum;
